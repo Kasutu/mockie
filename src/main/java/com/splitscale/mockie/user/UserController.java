b482d74054c2b22e718e203bfd2e0a509d340147
kasutu <jeromecabugwason2@protonmail.com>
2022-12-06 21:58:51 +0800

    add cors mappings

--- src/main/java/com/splitscale/mockie/user/UserController.java
+++ src/main/java/com/splitscale/mockie/user/UserController.java
@@ -3,6 +3,7 @@ package com.splitscale.mockie.user;
 import java.util.Map;
 import java.util.Random;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,10 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+  @Bean
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*");
+  }
 
   @ResponseBody
   @PostMapping(path = "/create")
