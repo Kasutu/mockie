4ad054826fc0ccb57c70ae693d931d0c209d813f
kasutu <jeromecabugwason2@protonmail.com>
2022-12-06 22:24:57 +0800

    update

--- src/main/java/com/splitscale/mockie/user/UserController.java
+++ src/main/java/com/splitscale/mockie/user/UserController.java
@@ -5,6 +5,7 @@ import java.util.Random;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RequestMapping("/api/user")
 public class UserController {
 
