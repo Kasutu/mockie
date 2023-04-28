32a798256432fd886044d636dace9b2916c438b6
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-28 14:33:25 +0800

    patch main

--- src/main/java/com/splitscale/mockie/MockieApplication.java
+++ src/main/java/com/splitscale/mockie/MockieApplication.java
@@ -2,12 +2,16 @@ package com.splitscale.mockie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.splitscale.mockie.config.RequestLoggingFilterConfig;
 
 @SpringBootApplication
+@Import(RequestLoggingFilterConfig.class)
 public class MockieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MockieApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MockieApplication.class, args);
+  }
 
 }
