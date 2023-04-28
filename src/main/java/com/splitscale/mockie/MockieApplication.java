35e73914c06035158e2db6674b6f8ecede8fa413
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-28 16:21:11 +0800

    fixed interceptor

--- src/main/java/com/splitscale/mockie/MockieApplication.java
+++ src/main/java/com/splitscale/mockie/MockieApplication.java
@@ -2,12 +2,8 @@ package com.splitscale.mockie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import com.splitscale.mockie.config.RequestLoggingFilterConfig;
 
 @SpringBootApplication
-@Import(RequestLoggingFilterConfig.class)
 public class MockieApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/splitscale/mockie/user/UserConfig.java
+++ src/main/java/com/splitscale/mockie/user/UserConfig.java
@@ -1,23 +1,23 @@
 package com.splitscale.mockie.user;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class UserConfig extends WebMvcConfigurationSupport {
 
-  private LocaleChangeInterceptor loggingInterceptor;
+  private HandlerInterceptor interceptor;
 
-  public UserConfig(LocaleChangeInterceptor loggingInterceptor) {
-    this.loggingInterceptor = loggingInterceptor;
+  public UserConfig(HandlerInterceptor interceptor) {
+    this.interceptor = interceptor;
   }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
-    InterceptorRegistration registration = registry.addInterceptor(loggingInterceptor);
+    InterceptorRegistration registration = registry.addInterceptor(interceptor);
     registration.addPathPatterns("/**");
   }
 
