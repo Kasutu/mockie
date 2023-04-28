8c871f6be8323a99dcec4fb514cf8f03478bfc57
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-28 13:07:23 +0800

    add web config

--- src/main/java/com/splitscale/mockie/config/WebConfig.java
+++ src/main/java/com/splitscale/mockie/config/WebConfig.java
@@ -1,18 +1,30 @@
 package com.splitscale.mockie.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import java.io.IOException;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurationSupport {
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class WebConfig extends OncePerRequestFilter {
 
   @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**").allowedOrigins("http://localhost:8080")
-        .allowedMethods("PUT", "DELETE", "POST", "GET")
-        .allowCredentials(false).maxAge(3600);
+  protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+      final FilterChain filterChain) throws ServletException, IOException {
+    response.addHeader("Access-Control-Allow-Origin", "*");
+    response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD, OPTIONS");
+    response.addHeader("Access-Control-Allow-Headers",
+        "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    response.addHeader("Access-Control-Expose-Headers",
+        "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization");
+    response.addHeader("Access-Control-Allow-Credentials", "true");
+    response.addIntHeader("Access-Control-Max-Age", 10);
+
+    filterChain.doFilter(request, response);
   }
 }
