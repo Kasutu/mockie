8a79a10455541bdf911524abab184fcd234820d8
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-28 18:05:17 +0800

    add loglemon

--- src/main/java/com/splitscale/mockie/config/LoggingFilter.java
+++ src/main/java/com/splitscale/mockie/config/LoggingFilter.java
@@ -1,10 +1,14 @@
 package com.splitscale.mockie.config;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import com.splitscale.Loglemon;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -17,27 +21,57 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class LoggingFilter implements Filter {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
+  private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
+
     HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-    // Log incoming request
-    LOGGER.info("Incoming request {} {}", httpRequest.getMethod(), httpRequest.getRequestURI());
+    String requestPayload = null;
+    if ("POST".equalsIgnoreCase(httpRequest.getMethod()) ||
+        "PUT".equalsIgnoreCase(httpRequest.getMethod())) {
+      requestPayload = getRequestPayload(httpRequest);
+    }
+
+    long startTime = System.currentTimeMillis();
 
     try {
-      // Call next filter (if any) or target servlet/controller
       chain.doFilter(request, response);
-    } catch (Exception e) {
-      // Log any errors
-      LOGGER.error("Error processing request: {}", e.getMessage(), e);
-      throw e;
     } finally {
-      // Log outgoing response
-      LOGGER.info("Outgoing response with status {}", httpResponse.getStatus());
+      long endTime = System.currentTimeMillis();
+      long duration = endTime - startTime;
+
+      String logMessage = String.format("[HTTP %d] %s %s (%d ms) | Request Payload: %s | Response Payload: %s",
+          httpResponse.getStatus(),
+          httpRequest.getMethod(),
+          httpRequest.getRequestURI(),
+          duration,
+          requestPayload,
+          getResponsePayload(httpResponse));
+
+      logger.info(logMessage);
+      Loglemon.sendLog(logMessage);
+    }
+  }
+
+  private String getRequestPayload(HttpServletRequest request) {
+    try {
+      return StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      logger.error("Error reading request payload", e);
+      return null;
+    }
+  }
+
+  private String getResponsePayload(HttpServletResponse response) {
+    try {
+      return response.getOutputStream().toString();
+    } catch (IOException e) {
+      logger.error("Error reading response payload", e);
+      return null;
     }
   }
 
