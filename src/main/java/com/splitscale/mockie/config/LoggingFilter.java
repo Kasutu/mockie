0602516ac5e2a032ebda1947d5552dc245369738
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-29 12:13:00 +0800

    patch filter formatting

--- src/main/java/com/splitscale/mockie/config/LoggingFilter.java
+++ src/main/java/com/splitscale/mockie/config/LoggingFilter.java
@@ -1,22 +1,28 @@
 package com.splitscale.mockie.config;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StreamUtils;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import com.splitscale.Loglemon;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.WriteListener;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 
 @Component
 public class LoggingFilter implements Filter {
@@ -24,55 +30,84 @@ public class LoggingFilter implements Filter {
   private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-    HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-    String requestPayload = null;
-    if ("POST".equalsIgnoreCase(httpRequest.getMethod()) ||
-        "PUT".equalsIgnoreCase(httpRequest.getMethod())) {
-      requestPayload = getRequestPayload(httpRequest);
-    }
-
-    long startTime = System.currentTimeMillis();
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
 
     try {
-      chain.doFilter(request, response);
-    } finally {
-      long endTime = System.currentTimeMillis();
-      long duration = endTime - startTime;
+      HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-      String logMessage = String.format("[HTTP %d] %s %s (%d ms) | Request Payload: %s | Response Payload: %s",
-          httpResponse.getStatus(),
+      ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(httpRequest);
+      String requestPayload = getContentAsString(requestWrapper.getContentAsByteArray(),
+          requestWrapper.getCharacterEncoding());
+
+      class CustomResponseWrapper extends HttpServletResponseWrapper {
+        private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        private final ServletOutputStream outputStream = new ServletOutputStream() {
+          @Override
+          public void write(int b) throws IOException {
+            baos.write(b);
+          }
+
+          @Override
+          public boolean isReady() {
+            throw new UnsupportedOperationException("Unimplemented method 'isReady'");
+          }
+
+          @Override
+          public void setWriteListener(WriteListener arg0) {
+            throw new UnsupportedOperationException("Unimplemented method 'setWriteListener'");
+          }
+        };
+
+        public CustomResponseWrapper(HttpServletResponse response) {
+          super(response);
+        }
+
+        @Override
+        public ServletOutputStream getOutputStream() throws IOException {
+          return outputStream;
+        }
+
+        @Override
+        public PrintWriter getWriter() throws IOException {
+          return new PrintWriter(baos);
+        }
+
+        @Override
+        public void flushBuffer() throws IOException {
+          super.flushBuffer();
+          baos.flush();
+        }
+
+        public byte[] getResponseAsByteArray() {
+          return baos.toByteArray();
+        }
+      }
+
+      CustomResponseWrapper responseWrapper = new CustomResponseWrapper((HttpServletResponse) response);
+      chain.doFilter(requestWrapper, responseWrapper);
+
+      String responsePayload = getContentAsString(responseWrapper.getResponseAsByteArray(),
+          responseWrapper.getCharacterEncoding());
+
+      String logMessage = String.format("%s %s %s | requestPayload=%s | responsePayload=%s",
           httpRequest.getMethod(),
           httpRequest.getRequestURI(),
-          duration,
+          responseWrapper.getStatus(),
           requestPayload,
-          getResponsePayload(httpResponse));
+          responsePayload);
 
       logger.info(logMessage);
       Loglemon.sendLog(logMessage);
+    } catch (Exception e) {
+      final String message = "[LoggingFilter] [ERROR]: " + e.getMessage();
+      logger.error(message);
+      Loglemon.sendLog(message);
     }
+
   }
 
-  private String getRequestPayload(HttpServletRequest request) {
-    try {
-      return StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
-    } catch (IOException e) {
-      logger.error("Error reading request payload", e);
-      return null;
-    }
-  }
-
-  private String getResponsePayload(HttpServletResponse response) {
-    try {
-      return response.getOutputStream().toString();
-    } catch (IOException e) {
-      logger.error("Error reading response payload", e);
-      return null;
-    }
+  private String getContentAsString(byte[] content, String charsetName) throws UnsupportedEncodingException {
+    return new String(content, charsetName);
   }
 
 }
