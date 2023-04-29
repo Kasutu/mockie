6cb0392c0c543d77b69c1b06e3afcb832be9ad73
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-04-29 12:57:25 +0800

    update filter formatting

--- src/main/java/com/splitscale/mockie/config/LoggingFilter.java
+++ src/main/java/com/splitscale/mockie/config/LoggingFilter.java
@@ -4,18 +4,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.splitscale.Loglemon;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
@@ -29,9 +31,7 @@ public class LoggingFilter implements Filter {
 
   private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
-  @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
-
     try {
       HttpServletRequest httpRequest = (HttpServletRequest) request;
 
@@ -89,21 +89,44 @@ public class LoggingFilter implements Filter {
       String responsePayload = getContentAsString(responseWrapper.getResponseAsByteArray(),
           responseWrapper.getCharacterEncoding());
 
-      String logMessage = String.format("%s %s %s | requestPayload=%s | responsePayload=%s",
-          httpRequest.getMethod(),
-          httpRequest.getRequestURI(),
-          responseWrapper.getStatus(),
-          requestPayload,
-          responsePayload);
+      // Build log message
+      Map<String, Object> logMessage = new HashMap<>();
+      logMessage.put("filter", logger.getName());
+      logMessage.put("level", "ERROR");
+      logMessage.put("method", httpRequest.getMethod());
+      logMessage.put("uri", httpRequest.getRequestURI());
+      logMessage.put("status", responseWrapper.getStatus());
+      logMessage.put("requestPayload", requestPayload);
+      logMessage.put("responsePayload", responsePayload);
 
-      logger.info(logMessage);
-      Loglemon.sendLog(logMessage);
+      String logJson = new ObjectMapper().writeValueAsString(logMessage);
+
+      logger.info(logJson);
+      Loglemon.sendLog(logJson);
+
     } catch (Exception e) {
-      final String message = "[LoggingFilter] [ERROR]: " + e.getMessage();
-      logger.error(message);
-      Loglemon.sendLog(message);
+      handleException(e);
     }
+  }
 
+  private void handleException(Exception e) {
+    try {
+      if (e != null) {
+        // Build error message
+        Map<String, Object> errorMessage = new HashMap<>();
+        errorMessage.put("filter", logger.getName());
+        errorMessage.put("level", "ERROR");
+        errorMessage.put("message", e.getMessage());
+
+        String errorJson = new ObjectMapper().writeValueAsString(errorMessage);
+
+        logger.error(errorJson);
+        Loglemon.sendLog(errorJson);
+      }
+    } catch (JsonProcessingException ex) {
+      logger.error(ex.getMessage());
+      Loglemon.sendLog(ex.getMessage());
+    }
   }
 
   private String getContentAsString(byte[] content, String charsetName) throws UnsupportedEncodingException {
